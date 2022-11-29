de43cda9f1eb00d9a0647bc7db3ee39fbaf97360
kimyb8870 <qhd0084@gmail.com>
2022-11-29 13:09:13 +0900

    Test: 로그인 관련 문제로 interceptor 관련 적용 안되도록 설정

--- src/main/java/com/example/demo/util/WebConfig.java
+++ src/main/java/com/example/demo/util/WebConfig.java
@@ -1,19 +1,23 @@
 package com.example.demo.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Autowired
-	LoginInterceptor loginInterceptor;
+//	@Autowired
+//	LoginInterceptor loginInterceptor;
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginInterceptor).addPathPatterns(loginInterceptor.loginEssential)
-				.excludePathPatterns(loginInterceptor.loginInessential);
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(loginInterceptor).addPathPatterns(loginInterceptor.loginEssential)
+//				.excludePathPatterns(loginInterceptor.loginInessential);
+//	}
+
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowCredentials(true);
+//	}
+
 }
