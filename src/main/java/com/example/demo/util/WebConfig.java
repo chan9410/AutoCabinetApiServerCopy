f2c5fcecf4c10bab46e987590a7e3f30d9cd09ac
chan <chan941027@gmail.com>
2022-12-06 15:48:17 +0900

    FIX Login Interceptor 주석 처리

--- src/main/java/com/example/demo/util/WebConfig.java
+++ src/main/java/com/example/demo/util/WebConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	LoginInterceptor loginInterceptor;
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginInterceptor).addPathPatterns(loginInterceptor.loginEssential)
-				.excludePathPatterns(loginInterceptor.loginInessential);
-	}
+	/*
+	 * @Override public void addInterceptors(InterceptorRegistry registry) {
+	 * registry.addInterceptor(loginInterceptor).addPathPatterns(loginInterceptor.
+	 * loginEssential) .excludePathPatterns(loginInterceptor.loginInessential); }
+	 */
 }
